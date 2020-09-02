b119f0a1302713a237a7c3ca668885daeb9e5b61
marcelroed <marcelroed@gmail.com>
2020-09-02 18:20:37 +0200

    run_problems returns stream of finished results

--- src/checker.rs
+++ src/checker.rs
@@ -1,11 +1,15 @@
+use futures::future::join;
 use futures::future::join_all;
 use std::error::Error;
 use std::fs::File;
+use std::str::from_utf8;
 use tokio::process::{Child, Command};
 use tokio::spawn;
 
 use crate::fetch::{fetch_problem, ProblemIO};
 use crate::Result;
+use futures::prelude::stream::*;
+use futures::stream::{StreamExt, TryStreamExt};
 use futures::TryFutureExt;
 use std::convert::TryInto;
 use std::ffi::OsStr;
@@ -95,35 +99,39 @@ impl Program {
                 Lang::Cpp => Command::new(bin)
                     .stdin(Stdio::piped())
                     .spawn()
-                    .map_err(|_| "".into()),
+                    .map_err(|_| "Failed to spawn C++ program".into()),
                 Lang::Python => Command::new("python")
                     .arg("bin")
                     .stdin(Stdio::piped())
                     .spawn()
-                    .map_err(|_| "".into()),
+                    .map_err(|_| "Failed to spawn Python program".into()),
             }
         } else {
             Err("Program not compiled".into())
         }
     }
 
-    async fn run_problem(&self, i: &String) -> std::result::Result<Output, Output> {
+    async fn run_problem(&self, i: &String) -> Result<Output> {
         match self.spawn_process() {
             Ok(mut child) => {
-                child.stdin.as_mut().unwrap().write(i.as_bytes());
+                child.stdin.as_mut().unwrap().write(i.as_bytes()).await?;
                 Ok(child.wait_with_output().await.unwrap())
             }
-            _ => panic!(),
+            Err(e) => Err(e),
         }
     }
 
-    pub async fn run_problems(&self, ios: &Vec<ProblemIO>) -> Vec<Result<Output>> {
-        let mut results = Vec::with_capacity(ios.len());
+    pub fn run_problems(
+        &'a self,
+        ios: &'a Vec<ProblemIO>,
+    ) -> Result<impl Stream<Item = Result<Output>> + 'a> {
+        let mut tasks = FuturesUnordered::new();
         for (i, pio) in ios.into_iter().enumerate() {
             let input = &pio.input;
-            self.run_problem(input);
+            let task = self.run_problem(input);
+            tasks.push(task);
         }
-        results
+        Ok(tasks)
     }
 }
 
@@ -153,43 +161,47 @@ pub fn find_source(problem_name: &str) -> Result<Vec<PathBuf>> {
 /// Compiles, fetches, runs and compares problem
 async fn check_problem(problem_name: &str) -> Result<()> {
     println!("{}", problem_name);
+    //Ok(())
+    // Fetch problem IO
+    let future_io = fetch_problem(problem_name);
+
+    // Find source paths
+    let source = find_source(problem_name)?;
+    if source.is_empty() {
+        println!(
+            "Found no source code for problem {}. \
+        Make sure that the file exists with one of the supported extensions.\n",
+            problem_name
+        );
+    }
+
+    // Create programs
+    let mut programs: Vec<Program> = source
+        .into_iter()
+        .map(|s| Program::new(s).unwrap())
+        .collect();
+
+    // Compile programs and fetch io
+    let (_, io) = join(
+        join_all(programs.iter_mut().map(|p| p.compile())),
+        future_io,
+    )
+    .await;
+
+    let io = io?;
+
+    let io_borrow = &io;
+
+    // Run
+    let _run_results = join_all(programs.iter().map(async move |program| -> Result<()> {
+        let mut result_stream = program.run_problems(io_borrow)?;
+        while let Some(out) = result_stream.try_next().await? {
+            let output_string = from_utf8(out.stdout.as_slice())?;
+            println!("{}", output_string);
+        }
+        Ok(())
+    }));
+    // for ProblemIO { input, output } in io {}
+    println!("Checked problem {}", problem_name);
     Ok(())
-    // // Fetch problem IO
-    // let future_io = fetch_problem(problem_name);
-    //
-    // // Find source paths
-    // let source = find_source(problem_name)?;
-    // if source.is_empty() {
-    //     println!(
-    //         "Found no source code for problem {}. \
-    //     Make sure that the file exists with one of the supported extensions.\n",
-    //         problem_name
-    //     );
-    // }
-    //
-    // // Create programs
-    // let mut programs = source
-    //     .into_iter()
-    //     .map(|s| Program::new(s).unwrap())
-    //     .collect::<Vec<_>>();
-    //
-    // let io = future_io.await?;
-    //
-    // // Compile
-    // let future_compile = join_all(
-    //     programs
-    //         .into_iter()
-    //         .map(|mut p| p.compile().and_then(|_| p.run(&io))),
-    // )
-    // .await;
-    //
-    // // let future_run = future_compile.into_iter().map(|r| r.unwrap().run(&io));
-    //
-    // //let io = future_io.await?;
-    //
-    // // Run
-    // let run_results = programs.iter_mut().map(|program| program.run(&io));
-    // // for ProblemIO { input, output } in io {}
-    // println!("Checked problem {}", problem_name);
-    // Ok(())
 }
