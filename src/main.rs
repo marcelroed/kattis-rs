7e8a9e1d8ab9e3230610cd407d63b54858c079bf
marcelroed <marcelroed@gmail.com>
2020-09-06 22:02:41 +0200

    Include cargo version, better description of arguments

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ mod fetch;
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error + Send + Sync + 'static>>;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 #[tokio::main]
 pub async fn main() {
     // Create folder in tmp if it doesn't already exist
@@ -19,14 +21,20 @@ pub async fn main() {
 
     std::fs::create_dir_all(kattis_temp).unwrap();
     let mut app = App::new("Kattis Tester")
-        .version("0.1")
+        .version(VERSION)
         .author("Marcel Rød")
         .about("Tests Kattis competitive programming problems.")
-        .arg(Arg::new("problems")
-            .about("Names of the problems to test. The format needs to be {problem} in open.kattis.com/problems/{problem}")
-            .required(false)
-            .multiple(true)
-            .value_name("PROBLEMS")
+        .arg(
+            Arg::new("problems")
+                .about(
+                    "Names of the problems to test.\
+                    The format needs to be {problem} in open.kattis.com/problems/{problem}. \
+                    If left empty, the problem name will be the name of the last edited source file. \
+                    Make sure that source files use the file name stem {problem}.",
+                )
+                .required(false)
+                .multiple(true)
+                .value_name("PROBLEMS"),
         );
     let matches = app.get_matches_mut();
 
