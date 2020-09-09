59d554fdcff5ec90e4777c9d78a35a88e0b5e942
marcelroed <marcelroed@gmail.com>
2020-09-09 02:13:48 +0200

    Caching methods for everything fetched from Kattis

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,7 +1,17 @@
 use crate::Result;
 use std::collections::HashMap;
 
-use std::io::{Read, Write};
+use futures::io::SeekFrom;
+use std::env::temp_dir;
+use std::io::Read;
+use tokio::fs::{File, OpenOptions};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, ErrorKind};
+
+pub fn initialize_temp_dir() -> Result<()> {
+    let mut tmp_dir = std::env::temp_dir();
+    tmp_dir.push("kattis/problem_files/");
+    std::fs::create_dir_all(tmp_dir).map_err(|e| e.into())
+}
 
 #[derive(Debug, Clone)]
 pub struct ProblemIO {
@@ -35,21 +45,44 @@ fn remove_suffix(s: &str, p: Vec<&str>) -> String {
 
 pub async fn fetch_problem(problem_name: &str) -> Result<Vec<ProblemIO>> {
     // Fetch from Kattis
-    let mut tmpfile = tempfile::tempfile().unwrap();
-    let tmp = reqwest::get(
-        format!(
-            "https://open.kattis.com/problems/{}/file/statement/samples.zip",
-            problem_name
-        )
-        .as_str(),
-    )
-    .await?
-    .bytes()
-    .await?;
+    let mut problem_path = std::env::temp_dir();
+    problem_path.push(format!("kattis/problem_files/{}.zip", problem_name));
 
-    tmpfile.write_all(&*tmp)?;
+    let mut problem_file = match File::open(&problem_path).await {
+        Ok(f) => f,
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => {
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .read(true)
+                    .create(true)
+                    .open(&problem_path)
+                    .await?;
 
-    let mut zip = zip::ZipArchive::new(tmpfile).unwrap();
+                let tmp = reqwest::get(
+                    format!(
+                        "https://open.kattis.com/problems/{}/file/statement/samples.zip",
+                        problem_name
+                    )
+                    .as_str(),
+                )
+                .await?
+                .bytes()
+                .await?;
+
+                file.write_all(&*tmp).await?;
+                file.seek(SeekFrom::Start(0)).await?;
+                file
+            }
+            _ => return Err(e.into()),
+        },
+    };
+
+    let mut file_contents = Vec::with_capacity(problem_file.metadata().await?.len() as usize);
+    problem_file.read_buf(&mut file_contents).await?;
+    let cursor = std::io::Cursor::new(file_contents);
+
+    let mut zip = zip::ZipArchive::new(cursor)?;
     let mut file_names: Vec<_> = zip.file_names().map(String::from).collect();
     file_names.sort();
 
@@ -81,6 +114,22 @@ pub async fn fetch_problem(problem_name: &str) -> Result<Vec<ProblemIO>> {
 }
 
 pub async fn problem_exists(problem_name: &str) -> Result<bool> {
+    let mut problem_path = temp_dir();
+    problem_path.push("kattis/problem_files/");
+    let problem_names: Vec<_> = walkdir::WalkDir::new(problem_path)
+        .max_depth(1)
+        .into_iter()
+        .map(|f| {
+            let de = f.unwrap();
+            let s = de.file_name().to_str().unwrap();
+            s[..s.len() - 3].to_owned()
+        })
+        .collect();
+
+    if problem_names.iter().any(|pn| pn == problem_name) {
+        return Ok(true);
+    }
+
     let str = reqwest::get(format!("https://open.kattis.com/problems/{}", problem_name).as_str())
         .await?
         .text()
